48f9683ff2f719ff00554f197425b63ff3d5609c
tatigonch <tanya.avror@gmail.com>
2022-07-25 15:26:15 +0300

    feat: add fouth task solution

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -1,6 +1,6 @@
 /* *******************************************************************************************
  *                                                                                           *
- * Please read the following tutorial before implementing tasks:                              *
+ * Plese read the following tutorial before implementing tasks:                              *
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Numbers_and_dates#Date_object
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date     *
  *                                                                                           *
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,13 +53,15 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const dayFromDate = date.getDate();
+  date.setDate(dayFromDate + 28);
+  return date.getMonth() === 1;
 }
 
 
 /**
- * Returns the string representation of the timespan between two dates.
+ * Returns the string represention of the timespan between two dates.
  * The format of output string is "HH:mm:ss.sss"
  *
  * @param {date} startDate
@@ -73,8 +75,13 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const ms = new Date(endDate - startDate);
+  const hours = ms.getUTCHours().toLocaleString('en', { minimumIntegerDigits: 2 });
+  const minutes = ms.getMinutes().toLocaleString('en', { minimumIntegerDigits: 2 });
+  const seconds = ms.getSeconds().toLocaleString('en', { minimumIntegerDigits: 2 });
+  const msec = ms.getMilliseconds().toLocaleString('en', { minimumIntegerDigits: 3 });
+  return `${hours}:${minutes}:${seconds}.${msec}`;
 }
 
 
@@ -94,8 +101,27 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const hours = date.getUTCHours() % 12;
+  let minutes;
+  if (hours > 6) {
+    minutes = 720 - hours * 60 - date.getMinutes();
+  } else {
+    minutes = hours * 60;
+  }
+
+  const mins = date.getMinutes();
+  let minutes2;
+  if (mins > 30) {
+    minutes2 = 720 - (mins * 720) / 60;
+  } else {
+    minutes2 = mins;
+  }
+
+  const diff = Math.abs(minutes - minutes2);
+
+  const deg = (diff * 360) / 720;
+  return (deg / 180) * Math.PI;
 }
 
 
